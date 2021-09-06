4c040c440f0774233e614920a792ccc846ffa9f3
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2021-09-06 17:40:23 +0200

    First commit

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>jdjdkj</h1>
+    </div>
+  );
+}
+
+export default App;
