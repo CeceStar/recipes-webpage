b9908779e0df52cf66af5053b70ff2184c897988
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2021-09-09 16:47:38 +0200

    added all the items to the hero, but the positioning is not working as wanted.

--- src/components/Mainpage/hero.tsx
+++ src/components/Mainpage/hero.tsx
@@ -5,19 +5,16 @@ function Hero() {
   return (
     <div className="landing-hero-container">
       <div className="hide-the-wave"></div>
-      <div className="hero-images-container">
-        <div className="hero-images">
-          <HeroImageBlobb />
-          <img
-            className="hero-like"
-            src={LikeStamp}
-            alt="Drawn hand doing thumps up on a blue background"
-          />
-        </div>
+      <div className="hero-images">
+        <HeroImageBlobb />
+        <img
+          className="hero-like"
+          src={LikeStamp}
+          alt="Drawn hand doing thumps up on a blue background"
+        />
       </div>
-      <div className="super-title-container">
-        <h1 className="super-title">Lets eat and be happy</h1>
-      </div>
+      <h1 className="super-title">Lets eat and be happy</h1>
+      <button className="btn btn-hero">View all</button>
     </div>
   );
 }
