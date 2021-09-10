08224e57d2250d6641f33a3740357ff30b0d5a27
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2021-09-10 14:31:35 +0200

    Updates on the hero, created for mobile only

--- src/components/Mainpage/hero.tsx
+++ src/components/Mainpage/hero.tsx
@@ -1,20 +1,31 @@
-import HeroImageBlobb from "./blobb-hero-image";
+// import HeroImageBlobb from "./blobb-hero-image";
 import LikeStamp from "../../assets/images/Like-stamp.png";
+import { Link } from "react-router-dom";
 
 function Hero() {
   return (
     <div className="landing-hero-container">
       <div className="hide-the-wave"></div>
       <div className="hero-images">
-        <HeroImageBlobb />
-        <img
-          className="hero-like"
-          src={LikeStamp}
-          alt="Drawn hand doing thumps up on a blue background"
-        />
+        <div className="big-picture">
+          <img
+            className="hero-like"
+            src={LikeStamp}
+            alt="Drawn hand doing thumps up on a blue background"
+          />
+        </div>
+        {/* <HeroImageBlobb /> */}
       </div>
-      <h1 className="super-title">Lets eat and be happy</h1>
-      <button className="btn btn-hero">View all</button>
+      <h1 className="super-title">
+        Lets eat
+        <br />
+        and be
+        <br />
+        happy
+      </h1>
+      <Link to="/recipes">
+        <button className="btn btn-hero">View all</button>
+      </Link>
     </div>
   );
 }
