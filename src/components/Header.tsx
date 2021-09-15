a5983f9b08725571189c36e9a02ce655d9959a49
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2021-09-15 16:06:27 +0200

    updated the animation of the header nav bar so it is animated both when opening and closing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,22 +5,6 @@ import WavyLine from "../assets/images/header-wave.svg";
 function Header() {
   const [open, setOpen] = useState(false);
 
-  const navbarBg = document.getElementById("hamburger-btn-bg");
-  const navbarBtns = document.getElementById("hamburger-nav");
-
-  function showNavbar(): void {
-    if (navbarBg !== null && navbarBtns !== null) {
-      navbarBg.style.display = "block";
-      navbarBtns.style.display = "block";
-    }
-  }
-  function hideNavbar(): void {
-    if (navbarBg !== null && navbarBtns !== null) {
-      navbarBg.style.display = "none";
-      navbarBtns.style.display = "none";
-    }
-  }
-
   return (
     <>
       <div className="background-header">
@@ -34,24 +18,24 @@ function Header() {
             <button className="header-btn">About me</button>
           </div>
           <div className="hamburger-nav-container">
-            <div id="hamburger-btn-bg"></div>
+            <div
+              className={
+                open ? "hamburger-btn-bg" : "hamburger-btn-bg-close"
+              }></div>
             <button
               id="hamburger-btn"
               onClick={() => {
                 setOpen(!open);
-                if (open === true) {
-                  showNavbar();
-                } else {
-                  hideNavbar();
-                }
               }}>
               {open ? (
-                <i className="fas fa-bars"></i>
-              ) : (
                 <i className="fas fa-times"></i>
+              ) : (
+                <i className="fas fa-bars"></i>
               )}
             </button>
-            <div id="hamburger-nav">
+            <div
+              id="hamburger-nav"
+              style={open ? { display: "block" } : { display: "none" }}>
               <ul>
                 <li>
                   <button className="hamburger-list-btn">Home</button>
